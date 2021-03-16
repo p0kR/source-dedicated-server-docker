26c0a606b6258bee5c01aac2d914bfc2864ccb54
26470877+p0kR@users.noreply.github.com <572829-p0kR@users.noreply.gitlab.com>
2021-03-16 01:17:35 +0100

    add script to copy config templates

---
+++ copy_initial_cfg.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cp configs_template/gmod/* root-dir/gmod_ds/garrysmod/cfg/
+cp configs_template/css/* root-dir/css_ds/cstrike/cfg/
+cp configs_template/tf2/* root-dir/tf2_ds/tf/cfg/
+cp configs_template/hl2dm/* root-dir/hl2dm_ds/hl2mp/cfg/
